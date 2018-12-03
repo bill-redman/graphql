467b1edabf07395dff9109443fcbd07f3b1c7057
Bill <william.redman@stonybrookmedicine.edu>
2018-12-03 11:43:42 -0500

    changes

--- clientside.js
+++ clientside.js
@@ -1,26 +1,35 @@
 const theurl = "https://brgraphql.herokuapp.com/"
 
-method: "POST"
-Host: brgraphql.herokuapp.com
-Accept-Encoding: gzip, deflate, br
-Content-Type: application/json
-Accept: application/json
-Connection: keep-alive
-DNT: 1
-Origin: https://brgraphql.herokuapp.com
-cache-control: no-cache
-Postman-Token: bab2bedd-c845-42c9-b6b7-27915a2551ce
+clientside = function() {
+    var arg1 = 
+    {
+        records(arg1:"facility_name=University Hospital&$limit=10") {
+            facility_name
+            gender
+            total_charges
+            ccs_diagnosis_description
+            health_service_area
+            total_costs
+          }
+    }
 
-method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            // "Content-Type": "application/x-www-form-urlencoded",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrer: "no-referrer", // no-referrer, *client
-        body: 
 
-        {"query":"query {\n  records(arg1:\"facility_name=Cuba Memorial Hospital Inc&$limit=5\") {\n    facility_name\n    total_costs\n    age_group\n    discharge_year\n  }\n}\n"}
+data = JSON.stringify({query:arg1})
+
+fetch(theurl,
+    {
+      method:'POST',
+      body:data,
+      headers:{
+        "Content-Type": "application/json",
+        //"Accept": "application/json",
+        //"cache-control": "no-cache"
+      }
+    }
+)
+.then(response => response.json())
+.then(json => console.log(json));
+
+}
+
+clientside()
