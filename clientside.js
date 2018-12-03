90318b6dccdbcc5cd6f205e261e5547c67919529
Bill <william.redman@stonybrookmedicine.edu>
2018-12-03 14:57:00 -0500

    changes

--- clientside.js
+++ clientside.js
@@ -1,9 +1,9 @@
-const theurl = 'https://brgraphql.herokuapp.com/';
+const theurl = "https://brgraphql.herokuapp.com/";
 
 clientside = function() {
     var arg1 = 
     `{
-        records(arg1:'facility_name=University Hospital&$limit=10') {
+        records(arg1:"facility_name=University Hospital&$limit=10") {
             facility_name
             gender
             total_charges
