8ade97fa60f239f3e43db7e43f894a7fbad22751
Bill <william.redman@stonybrookmedicine.edu>
2019-01-03 12:50:10 -0500

    change

--- index.js
+++ index.js
@@ -39,13 +39,7 @@ const typeDefs = gql`
   type DocInfo {
     npi: String
     officeName: String
-    address {
-      line1: String
-      line2: String
-      city: String
-      region: String
-      postalCode: String
-    }
+    address: String
     localPhone: String
   }
 
