c0a290333f1c4ce9313cc8256d1f5cf78b66a654
Bill <william.redman@stonybrookmedicine.edu>
2019-01-04 13:21:26 -0500

    change

--- docinfo.js
+++ docinfo.js
@@ -4,7 +4,7 @@ docinfo = function() {
     var arg2 = 
     `{
         getdocinfo(arg2:"${theQuery.value}") {
-            locations 
+            locations [
             {
                 npi
                 name
@@ -28,7 +28,7 @@ docinfo = function() {
                     property1:{22884}
                     property2:{22817}
                 }
-            }
+            }]
           }
     }`
 
