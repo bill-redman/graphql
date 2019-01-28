de25e5b49f8e0a3af86cc916011e175f1dd0d7ac
Bill <william.redman@stonybrookmedicine.edu>
2019-01-28 14:46:42 -0500

    Changes

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const CustomType = new GraphQLObjectType({
   fields: {
     PersonalStatement: {
       type: GraphQLString,
-      resolve: (x) => x['22817'],
+      resolve: (this) => this['22817'],
     },
     Titles: {
       type: GraphQLString,
-      resolve: (x) => x['22884']
+      resolve: (this) => this['22884']
     }
   }
 });
